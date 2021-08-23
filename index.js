d6a164117d9e89e7e2f4b3e6d53fa2ef41a79312
Steven Brock <shyfay0@gmail.com>
2021-08-22 22:33:15 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(gifts, eventName) {
+  let thanks = []
+  for(let i=0; i < gifts.length; i++) {
+    let msg = `Thank you, ${gifts[i]}, for the wonderful ${eventName} gift!`
+    thanks.push(msg);
+
+  }
+  return thanks
+}
+
+
+function countDown(){
+  for(let i = 10; i > -1; i--){
+    console.log(i);
+  }
+}
